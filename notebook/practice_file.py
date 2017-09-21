0bb471444ff83c8850b1734a4401817db10678d0
Shuji Tamada <tamamoon@shuji-no-MacBook-Air.local>
2017-09-21 16:52:15 +0900

    another modificaiton at the seminar

--- notebook/practice_file.py
+++ notebook/practice_file.py
@@ -17,16 +17,30 @@ import argparse
 import pdb
 import os
 
-term=1#最終時刻
-step=0.1#刻み幅
-div=term/step#飛ぶ回数
-init=np.array([0.])#初期値
-#repeat_time=20000#pass本数
-#S=0
-mymodel = sde.SDE_Markov(mymat=np.array([[0.]]), myvar =np.array([[1.]]),myinit=init,myscale=1.,myterm=term,step_size=step)
+def main():
 
-simulate=bmt.brown_motion(mymat=np.array([[0.]]), myvar =np.array([[1.]]),myinit=init,myscale=1.,myterm=term,step_size=step)
-#transform_matrix_step=np.array([[0.]]),transform_matrix_noise=np.array([[1.]]),noise_var_matrix,terminal,\
-#deltaT,division,normal_mean,normal_scale,dimen,init
+    term=1#最終時刻
+    step=0.1#刻み幅
+    div=term/step#飛ぶ回数
+    init=np.array([0.])#初期値
+    #repeat_time=20000#pass本数
+    #S=0
+    sdekey={}
+    sdekey['default'] = True
+    sdekey['init'] = init
+    sdekey['stepsize'] = step
+    sdekey['term'] = term
+    sdekey['variance'] = 1.0
+    sdekey['mean'] = 1.0
 
-brown_motion_nomal=simulate.brown_motion_nomal()
+
+    #mymodel = sde.SDE_Markov(**sdekey)
+    simulate=bmt.brown_motion(**sdekey)
+
+    #transform_matrix_step=np.array([[0.]]),transform_matrix_noise=np.array([[1.]]),noise_var_matrix,terminal,\
+    #deltaT,division,normal_mean,normal_scale,dimen,init
+
+    #brown_motion_nomal=simulate.brown_motion_nomal()
+
+if __name__ == '__main__':
+    main()
